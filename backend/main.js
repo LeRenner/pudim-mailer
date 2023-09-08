87a0820461d3a08595670a5c78d0e5bff33640da
J. Renner <j@lerenner.dev>
2023-09-07 23:16:28 -0300

    Add support to message author on backend
    Signed-off-by: J. Renner <j@lerenner.dev>

--- backend/main.js
+++ backend/main.js
@@ -18,14 +18,23 @@ app.post('/api/sendMail', (req, res) => {
 	const receivedData = req.body;
 	console.log('Received JSON data:', receivedData);
 
-	if (receivedData.text.length < 1000 && receivedData.text.length > 0) {
+	if (receivedData.text.length < 1000 && receivedData.text.length > 0 && receivedData.name.length < 30) {
 		// Send data to MQTT broker
+
+		if (receivedData.name == undefined || receivedData.name == '') {
+			receivedData.name = 'Anonymous';
+		}
+
+		let messageText = receivedData.name + ' says: \n\n' + receivedData.text;
+
 		client.publish('pudimMail', receivedData.text);
 
 		res.send('Message sent!');
 	} else {
 		if (receivedData.text.length > 1000) {
 			res.send('The message cannot be longer than 1000 characters!');
+		} else if (receivedData.name.length >= 30) {
+			res.send('The name cannot be longer than 30 characters!');
 		} else {
 			res.send('The message cannot be empty!');
 		}
